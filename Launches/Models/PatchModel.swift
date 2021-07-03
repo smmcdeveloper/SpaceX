56ec5ff42bd5ace9553d32f7e391968bf237c820
smmcdeveloper <74007080+smmcdeveloper@users.noreply.github.com>
2021-07-03 14:58:43 +0100

    Add files via upload

---
+++ Launches/Models/PatchModel.swift
@@ -0,0 +1,15 @@
+//
+//  PatchModel.swift
+//  Launches
+//
+//  Created by SMMC on 02/07/2021.
+//
+
+import Foundation
+
+struct PatchModel: Decodable {
+    
+    let small: String?
+    let large: String?
+    
+}
