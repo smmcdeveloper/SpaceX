f8c44ba756c758038933a132a151bb8755082fc4
smmcdeveloper <74007080+smmcdeveloper@users.noreply.github.com>
2021-07-03 14:58:31 +0100

    Update LaunchesModel.swift

--- Launches/Models/LaunchesModel.swift
+++ Launches/Models/LaunchesModel.swift
@@ -6,25 +6,25 @@
 
 import Foundation
 
-struct LaunchModel : Codable {
-    var mission_name: String = ""
-    var launch_date_utc: String = ""
-    var launchDateformatted: String? {return usDateToUK(launch_date_utc) }
-    var land_success: Bool?
-    var links: Links
+struct LaunchModel: Decodable {
     
-    struct Links: Codable {
-        var mission_patch_small: String? = ""
-    }
-  
-    func usDateToUK (_ inpDate: String) -> String {
+    var name: String?
+    var date_local: String
+    var launchDateformatted: String {return usDateToUK(date_local)}
+    var success: Bool?
+    var links: LinkModel?
+    
+    func usDateToUK (_ inpDate: String?) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-       
-        let date = dateFormatter.date(from: inpDate)
-        dateFormatter.dateFormat = "MM/dd/yyyy"
-      
-        return dateFormatter.string(from: date!) 
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        var dateSting: String = ""
+        
+        if let date = dateFormatter.date(from: inpDate ?? "") {
+            let displayFormater = DateFormatter()
+            displayFormater.dateFormat = "MMM dd, y"
+            dateSting = displayFormater.string(from: date)
+        }
+     
+        return dateSting
     }
 }
